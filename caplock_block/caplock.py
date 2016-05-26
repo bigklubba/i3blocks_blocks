bd0d2665e89c478908fe88db46a8eec43f46390c
Pierre Emrikstrand <pierre.emrikstrand@gmail.com>
2016-05-26 08:34:49 +0200

    added a block for checking status of capslock

---
+++ caplock_block/caplock.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+f = os.system('xset -q | grep "Caps Lock: *on" 1>/dev/null')
+print f
+if f == 0:
+  status = "on"
+  color = "#FF0000"
+else:
+  status = "off"
+  color = "#A8FF00"
+print status
+print status 
+print color
