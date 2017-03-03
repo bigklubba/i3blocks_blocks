20f2dcb99c4809db613f0ca6ac36c0190bb166da
Pierre Emrikstrand <pierre.emrikstrand@ist.com>
2017-03-03 12:31:32 +0100

    Added time left when discharging

--- i3bat_block/i3bat.py
+++ i3bat_block/i3bat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import subprocess
+from subprocess import Popen
 BAT_LEVEL = "/sys/class/power_supply/BAT0/capacity"
 BAT_CHARGING = "/sys/class/power_supply/BAT0/status"
 URGENT_LOWER = 15
@@ -14,6 +16,11 @@ ICON_BATTERY = ["", "", "", "", ""]
 def getBatteryIcon(bat_level):
     index = min(len(ICON_BATTERY)-1, bat_level//(100//len(ICON_BATTERY)))
     return ICON_BATTERY[index]
+
+
+def getTimeLeft():
+    output = Popen("acpi -b | awk '{ print $5 }'", stdout=subprocess.PIPE, shell=True).stdout.read()
+    return (output[0:5]).decode("utf-8")
 
 
 def isCharging():
@@ -32,9 +39,11 @@ def main():
     bat_level = int(bat_level_file.read())
     icon = ICON_DISCHARGING
     if isCharging():
-        icon = ICON_CHARGING
-    print("%s %s" % (icon, getBatteryIcon(bat_level)))
-    print("%s %s" % (icon, getBatteryIcon(bat_level)))
+        output = "%s %s" % (ICON_CHARGING, getBatteryIcon(bat_level))
+    else:
+        output = "%s (%s)" % (getBatteryIcon(bat_level), getTimeLeft())
+    print(output)
+    print(output)
     if bat_level < URGENT_LOWER:
         """ Make sure we get urgent notification to the i3bar """
         exit(33)
