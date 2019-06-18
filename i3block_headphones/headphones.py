8eac76996a777bc287025fc107c3c03ba79dcb2f
Pierre Emrikstrand <pierre.emrikstrand@fortnox.se>
2019-06-18 13:10:24 +0200

    only sound when headphones connected

--- i3block_headphones/headphones.py
+++ i3block_headphones/headphones.py
@@ -6,8 +6,10 @@ import os
 f = os.popen("bluetoothctl info | grep Connected")
 now = f.read()
 if 'yes' in now:
+    os.popen("amixer -q -D pulse sset Master unmute")
     color = "#42bff4"
 else:
+    os.popen("amixer -q -D pulse sset Master mute")
     color = "#FF0000"
 print('')
 print('')
